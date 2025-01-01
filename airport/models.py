7a61466b09be3d5521f0c0732d9ddc146209fb0d
Roman Sokolov <gnonasis@gmail.com>
2025-01-01 13:21:25 +0200

    implement basic  airport models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.db.models import ForeignKey
 
-# Create your models here.
+
+class AirplanType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Airplan(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats = models.IntegerField()
+    airplan_type = models.ForeignKey(
+        AirplanType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    def __str__(self):
+        return self.name
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="cities"
+    )
+    def __str__(self):
+        return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    closest_big_city = ForeignKey(
+        City, on_delete=models.CASCADE,
+        related_name="airports"
+    )
+
+    def __str__(self):
+        return (
+            f"{self.name}/{self.closest_big_city.name}/"
+                f"{self.closest_big_city.country}"
+        )
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source}-{self.destination}"
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.position} - {self.first_name} {self.last_name}"
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="flights"
+    )
+    airplan = models.ForeignKey(
+        Airplan, on_delete=models.CASCADE, related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class Order(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders"
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+
+
