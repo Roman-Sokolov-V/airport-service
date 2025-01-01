237fd266578c02a953ab73beae3b6579a2615406
Roman Sokolov <gnonasis@gmail.com>
2025-01-01 13:33:11 +0200

    add constraints to models Airport and Ticket

--- airport/models.py
+++ airport/models.py
@@ -19,8 +19,9 @@ class Airplan(models.Model):
     def __str__(self):
         return self.name
 
+
 class Country(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -41,6 +42,9 @@ class Airport(models.Model):
         City, on_delete=models.CASCADE,
         related_name="airports"
     )
+
+    class Meta:
+        unique_together = ("name", "closest_big_city")
 
     def __str__(self):
         return (
@@ -93,6 +97,9 @@ class Ticket(models.Model):
     order = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name="tickets"
     )
+
+    class Meta:
+        unique_together = ("row", "seat", "flight")
 
 
 
