ec6e577be02f9b4a0f9f8ebb0e049c7f63a44684
Roman Sokolov <gnonasis@gmail.com>
2025-01-04 18:21:12 +0200

    feat: add filtering by cities or/and airports (source-destination) in RouteViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.exceptions import ValidationError
 
 from airport.models import (
     AirplanType,
@@ -26,7 +27,8 @@ from airport.serializers import (
     FlightSerializer,
     TicketSerializer,
     OrderListSerializer,
-    OrderCreateSerializer
+    OrderCreateSerializer,
+    RouteListSerializer
 )
 
 from airport.permissions import IsAdminAllOrAuthenticatedReadOnly
@@ -72,8 +74,58 @@ class AirportViewSet(viewsets.ModelViewSet):
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
-    serializer_class = RouteSerializer
+    serializer_class = RouteListSerializer
     permission_classes = (IsAdminAllOrAuthenticatedReadOnly,)
+
+    @staticmethod
+    def split_params(params):
+        return params.split("-")
+
+    def get_queryset(self):
+        queryset = self.queryset
+        cities = self.request.query_params.get("cities")
+        airports = self.request.query_params.get("airports")
+
+        if cities:
+            try:
+                route = self.split_params(cities)
+                if len(route) != 2:
+                    raise ValidationError(
+                        {"cities": "Parameter 'cities' must contain exactly "
+                                   "two values separated by a '-'."}
+                    )
+                source, destination = route
+                source, destination = source.strip(), destination.strip()
+                queryset = queryset.filter(
+                    source__closest_big_city__name__iexact=source,
+                    destination__closest_big_city__name__iexact=destination
+                )
+            except Exception as e:
+                raise ValidationError({"cities": str(e)})
+
+        if airports:
+            try:
+                route = self.split_params(airports)
+                if len(route) != 2:
+                    raise ValidationError(
+                        {"airports": "Parameter 'airports' must contain "
+                                     "exactly two values separated by a '-'."}
+                    )
+                source, destination = route
+                source_id, destination_id = int(source.strip()), int(
+                    destination.strip())
+                queryset = queryset.filter(
+                    source__id=source_id,
+                    destination__id=destination_id
+                )
+            except ValueError:
+                raise ValidationError(
+                    {"airports": "Parameter 'airports' must contain valid integers."}
+                )
+            except Exception as e:
+                raise ValidationError({"airports": str(e)})
+
+        return queryset
 
 
 
